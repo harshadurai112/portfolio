83f3a8e03eabaa9f4ffef5a58e41082609f03185
harshadurai112 <harshavardhinidurai@gmail.com>
2024-07-17 12:49:45 +0530

    first commit

---
+++ port.html/port.js
@@ -0,0 +1,19 @@
+$(document).ready(function() {
+    // Smooth scrolling
+    $('a[href^="#"]').on('click', function(event) {
+        var target = $(this.getAttribute('href'));
+        if( target.length ) {
+            event.preventDefault();
+            $('html, body').stop().animate({
+                scrollTop: target.offset().top - 50 // Adjust for fixed header height
+            }, 1000);
+        }
+    });
+
+    // Image hover effect (optional)
+    $('.project img').hover(function() {
+        $(this).css('transform', 'scale(1.1)');
+    }, function() {
+        $(this).css('transform', 'scale(1)');
+    });
+});
